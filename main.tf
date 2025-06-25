c5819c556503a40f9fc6e7dfc5e00849a8142178
gvggvg <varun.guptha.3@gmail.com>
2025-06-25 14:33:41 +0530

    create s3 using tf script, .gitignore implemented

--- main.tf
+++ main.tf
@@ -2,4 +2,14 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "my-bucket" {
+    bucket="gvg.s3.bucket"
+
+}
+resource "aws_s3_bucket_versioning" "bucket-versio"{
+    bucket = aws_s3_bucket.my-bucket.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
 
