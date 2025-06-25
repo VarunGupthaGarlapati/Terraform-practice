be51725ebaa610729d406b047143fd4c21bcf4cf
gvggvg <varun.guptha.3@gmail.com>
2025-06-25 15:27:25 +0530

    added ebs volume in instance but this restarts the instance if we change size

--- main.tf
+++ main.tf
@@ -6,10 +6,27 @@ resource "aws_s3_bucket" "my-bucket" {
     bucket="gvg.s3.bucket"
 
 }
-resource "aws_s3_bucket_versioning" "bucket-versio"{
+resource "aws_s3_bucket_versioning" "bucket-version"{
     bucket = aws_s3_bucket.my-bucket.id
     versioning_configuration {
       status = "Enabled"
     }
 }
 
+resource "aws_instance" "my-instance" {
+  tags = {
+    Name= "Terraform-Ec2"
+  }
+  ami = "ami-000ec6c25978d5999"
+  instance_type = "t2.micro"
+  key_name = "keypairgvg"
+  availability_zone = "us-east-1a"
+  root_block_device{        // this is for os including,, ebs is used  for additional volume to add to instance
+    volume_size = 10
+  } 
+  count=1
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = 12
+  }
+}
